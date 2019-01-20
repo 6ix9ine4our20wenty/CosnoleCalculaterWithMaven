7ecd500542513d30663490f1477054a6fcc1170b
Livio <lilpeep.6ix9ine@gmail.com>
2019-01-20 16:21:55 +0100

    alle 8 Testfälle hinzufügen

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,6 +1,16 @@
 public class Calculator {
-    public int summe(int summand1, int summand2){
-        return summand1 + summand2;
+    int summe(int value1, int value2) {
+        long tester = (long) value1 + (long) value2;
+        overflowhandler(tester);
+        return (int) tester;
     }
 
+    private void overflowhandler(long tester) {
+        if (tester > Integer.MAX_VALUE) {
+            throw new ArithmeticException("Result is too big");
+        }
+        if (tester < Integer.MIN_VALUE) {
+            throw new ArithmeticException("Result is too small");
+        }
+    }
 }
