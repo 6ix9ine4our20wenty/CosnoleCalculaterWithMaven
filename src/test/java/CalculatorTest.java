8e2e8b375f5dc0753d47d5fce25a6f9cf0aec810
Livio <lilpeep.6ix9ine@gmail.com>
2019-01-20 16:59:09 +0100

    Subtraction Klasse mit Unittests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertEquals;
@@ -8,54 +9,79 @@ public class CalculatorTest{
         /*
     Addition
         */
+    @Before
+    public void setup(){
+        testee = new Calculator();
+    }
     @Test
     public void testSummePositive() {
-        testee = new Calculator();
         assertTrue(testee.summe(10, 20) == 30);
     }
     @Test
     public void testSummeNegative() {
-        testee = new Calculator();
         assertTrue(testee.summe(-20, -10) == -30);
     }
     @Test
     public void testSummePositiveNegative() {
-        testee = new Calculator();
         assertTrue(testee.summe(-10, 20) == 10);
     }
     @Test
     public void testSummeNegativePositive() {
-        testee = new Calculator();
         assertTrue(testee.summe(10, -20) == -10);
     }
     @Test
     public void testSummeZero() {
-        testee = new Calculator();
         assertTrue(testee.summe(0, 20) == 20);
     }
     @Test
     public void testSummeRounding() {
-        testee = new Calculator();
         assertTrue(testee.summe(5 / 2, 2) == 4);
     }
     @Test
     public void testSummeLargenumbers() {
-        testee = new Calculator();
         assertTrue(testee.summe(1000000, 2000000) == 3000000);
     }
     @Test(expected = ArithmeticException.class)
     public void testSummeOverflow() {
-       testee = new Calculator();
         assertEquals((long) Integer.MAX_VALUE + 1, testee.summe(Integer.MAX_VALUE, 1));
     }
         /*
     Subtraktion
         */
 
-        @Test
-    public void TestDifferenz(){
-            testee = new Calculator();
-            assertTrue(testee.differenz(2, 1) == 2);
-        }
+
+    @Test
+    public void testDifferenzPositive() {
+        assertTrue(testee.differenz(10, 20) == -10);
+    }
+    @Test
+    public void testDifferenzNegative() {
+        assertTrue(testee.differenz(-20, -10) == -10);
+    }
+    @Test
+    public void testDifferenzPositiveNegative() {
+        assertTrue(testee.differenz(-10, 20) == -30);
+    }
+    @Test
+    public void testDifferenzNegativePositive() {
+        assertTrue(testee.differenz(10, -20) == 30);
+    }
+    @Test
+    public void testDifferenzZero() {
+        assertTrue(testee.differenz(0, 20) == -20);
+    }
+    @Test
+    public void testDifferenzRounding() {
+        assertTrue(testee.differenz(5 / 2, 2) == 0);
+    }
+    @Test
+    public void testDifferenzLargenumbers() {
+        assertTrue(testee.differenz(1000000, 2000000) == -1000000);
+    }
+    @Test(expected = ArithmeticException.class)
+    public void testDifferenzOverflow() {
+        testee = new Calculator();
+        assertEquals((long) Integer.MIN_VALUE - 1, testee.differenz(Integer.MIN_VALUE, 1));
+    }
 
 }
